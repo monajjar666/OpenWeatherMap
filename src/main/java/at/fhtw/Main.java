4f427d2291d3d158e5f9bec825f679b615c13e47
yukarisoprano <yukarisoprano717@gmail.com>
2022-12-29 09:40:24 +0100

    Main.java changed to read API (Vienna) from OpenWeatherMap

--- src/main/java/at/fhtw/Main.java
+++ src/main/java/at/fhtw/Main.java
@@ -1,13 +1,30 @@
 package at.fhtw;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
 
+    public static void main(String[] args) throws Exception {
 
+        String requestUrl = "http://api.openweathermap.org/data/2.5/weather?&APPID=f46c41d3ff9363cb6319493cbbf3e57b&q=Vienna";
+        // URL: a pointer to a "resource" on the World Wide Web
+        URL url = new URL(requestUrl);
+        // HttpURLConnection: to make a single request but the underlying network connection to the HTTP server
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET"); // GET returns data from the server.
+        conn.connect();
 
+        int responseCode = conn.getResponseCode(); //Gets the status code from an HTTP response message
 
-    public static void main(String[] args) {
-
-        OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient();
-
+        if (responseCode == HttpURLConnection.HTTP_OK){
+            InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+            System.out.println(br.readLine());
+        } else {
+            System.out.println("Something is wrong with connection!");
+        }
     }
 }
