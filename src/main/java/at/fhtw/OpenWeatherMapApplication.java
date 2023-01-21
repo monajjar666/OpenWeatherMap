ee02b511d25ebd2ce67bd17ad39922e9c6679ba7
Rumpelstilzkind <ne.mes@gmx.at>
2023-01-21 23:50:35 +0100

    adds a fxml file and the OpenWeatherMapApplication class

---
+++ src/main/java/at/fhtw/OpenWeatherMapApplication.java
@@ -0,0 +1,18 @@
+package at.fhtw;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class OpenWeatherMapApplication extends Application{
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlloader = new FXMLLoader(OpenWeatherMapApplication.class.getResource("open_weather_map.fxml"));
+        Scene scene = new Scene(fxmlloader.load(), 320, 240);
+        stage.setTitle("OpenWeather");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
